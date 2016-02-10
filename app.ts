146265e139fa8fb48032dd563c9f97b49f2a348f
Jess Chadwick <jesschadwick@gmail.com>
2016-02-10 01:03:46 -0500

    02_08_End

--- app.ts
+++ app.ts
@@ -1,10 +1,29 @@
-function add() {
-    var values = Array.prototype.splice.call(arguments, [1]),
-        total = 0;
+function calculate(action, ...values) {
+    var total = 0;
 
     for (var value of values) {
-        total += value;
+        switch(action) {
+            case 'add':
+                total += value;
+                break;
+            
+            case 'subtract':
+                total -= value;
+                break;
+        }
     }
 
     return total;
 }
+
+calculate('add', 1,2,3,4,5)
+
+
+var source = [3, 4, 5]
+var target = [ 1, 2, ...source, 6, 7 ]
+
+
+var list = [1,2,3]
+var toAdd = [4,5,6];
+
+list.push(...toAdd)
