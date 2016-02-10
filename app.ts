472a796762ad90d7847159751be1135bf323dd2a
Jess Chadwick <jesschadwick@gmail.com>
2016-02-10 01:04:10 -0500

    02_09_Start

--- app.ts
+++ app.ts
@@ -1,29 +1,9 @@
-function calculate(action, ...values) {
-    var total = 0;
-
-    for (var value of values) {
-        switch(action) {
-            case 'add':
-                total += value;
-                break;
-            
-            case 'subtract':
-                total -= value;
-                break;
-        }
-    }
-
-    return total;
+	var support = {
+	    'os_Windows': isSupported('Windows'),
+	    'os_iOS': isSupported('iOS'),
+	    'os_Android': isSupported('Android'),
+	}
+	
+	function isSupported(os) {
+	    return Math.random() >= 0.5;
 }
-
-calculate('add', 1,2,3,4,5)
-
-
-var source = [3, 4, 5]
-var target = [ 1, 2, ...source, 6, 7 ]
-
-
-var list = [1,2,3]
-var toAdd = [4,5,6];
-
-list.push(...toAdd)
