ca660cd69f8d745c564876fb6ff71e4f1a697ee8
Jess Chadwick <jesschadwick@gmail.com>
2016-02-10 00:24:51 -0500

    02_07_Start

--- app.ts
+++ app.ts
@@ -1,15 +1,1 @@
-var container = document.getElementById('container');
-
-function Counter(el) {
-
-    this.count = 0;
-
-    el.innerHTML = this.count;
-
-    el.addEventListener('click', 
-        (event) => el.innerHTML = (this.count += 1))
-}
-
-new Counter(container);
-
-var filtered = [1,2,3].filter(x => x > 0)
+var array = [123, "Pick up drycleaning", false];
