5610f1cc64e5748851e214d7641a39b75ff5b608
Jess Chadwick <jesschadwick@gmail.com>
2016-02-10 01:09:31 -0500

    02_09_End

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
-	var support = {
-	    'os_Windows': isSupported('Windows'),
-	    'os_iOS': isSupported('iOS'),
-	    'os_Android': isSupported('Android'),
-	}
-	
-	function isSupported(os) {
-	    return Math.random() >= 0.5;
+const osPrefix = 'os_';
+
+var support = {
+    [osPrefix + 'Windows']: isSupported('Windows'),
+    [osPrefix + 'iOS']: isSupported('iOS'),
+    [osPrefix + 'Android']: isSupported('Android'),
+}
+
+function isSupported(os) {
+    return Math.random() >= 0.5;
 }
