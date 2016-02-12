393023247899cd1f8d66676f47bd66178feab557
Jess Chadwick <jesschadwick@gmail.com>
2016-02-12 01:58:44 -0500

    04_03_End

--- app.ts
+++ app.ts
@@ -11,10 +11,34 @@ interface Todo {
 }
 
 interface jQuery {
-    (selector: (string | any)): HTMLElement;
+    (selector: (string | any)): jQueryElement;
     fn: any;
     version: number;
 }
 
+interface jQueryElement {
+    data(name: string): any;
+    data(name: string, data: any): jQueryElement;
+}
+
+interface jQueryElement {
+    todo(): Todo;
+    todo(todo: Todo): jQueryElement;
+}
+
+$.fn.todo = function(todo?: Todo): Todo {
+    
+    if(todo) {
+        $(this).data('todo', todo)
+    } else {
+        return $(this).data('todo');
+    }
+    
+}
+
 var todo = { name: "Pick up drycleaning" };
 var container = $('#container');
+container.data('todo', todo)
+var savedTodo = container.data('todo');
+
+container.todo(todo)
