d2c61fe1a8926caa5b5a33ef98ef6257ee0c7fce
Jess Chadwick <jesschadwick@gmail.com>
2016-02-12 00:28:23 -0500

    04_03_Start

--- app.ts
+++ app.ts
@@ -1,13 +1,20 @@
-interface jQuery {
-    (selector: string): HTMLElement;
-    version: number;
-}
-
-
 var $ = <jQuery>function(selector: string) {
     // Find DOM element
 }
 
-$.version = 1.18
+$.version = 1.18;
 
+
+interface Todo {
+    name: string;
+    completed?: boolean;
+}
+
+interface jQuery {
+    (selector: (string | any)): HTMLElement;
+    fn: any;
+    version: number;
+}
+
+var todo = { name: "Pick up drycleaning" };
 var container = $('#container');
