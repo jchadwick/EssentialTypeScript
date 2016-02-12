ccae6db3fd4f6d427226baffd62d36d0819b8768
Jess Chadwick <jesschadwick@gmail.com>
2016-02-11 23:22:45 -0500

    03_03_Start

--- app.ts
+++ app.ts
@@ -1,9 +1,5 @@
-var animal = { 
-    name: "Fido",
-    species: "Dog",
-    age: 5,
-    speak: function() { 
-        console.log('Woof!'); 
-    }
-}
 
+function totalLength(x, y) {
+    var total = x.length + y.lengt;
+    return total;
+}
