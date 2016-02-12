889e62ac532ab9d498b82f7e0e5f5045c4fe1da7
Jess Chadwick <jesschadwick@gmail.com>
2016-02-12 00:05:22 -0500

    03_04_End

--- app.ts
+++ app.ts
@@ -1,5 +1,16 @@
 
-function totalLength(x: any[], y: string): number {
+function totalLength(x: (string | any[]), y: (string | any[])): number {
     var total: number = x.length + y.length;
+    
+    x.slice(0)
+    
+    if(x instanceof Array) {
+        x.push('TypeScript')
+    }
+    
+    if(x instanceof String) {
+        x.substr(0)
+    }
+    
     return total;
 }
