c954036237760f6b126af2cf692e44933b9653be
Jess Chadwick <jesschadwick@gmail.com>
2016-02-16 00:30:29 -0500

    05_04_End

--- app.ts
+++ app.ts
@@ -30,7 +30,39 @@ enum TodoState {
     Deleted
 }
 
-var todo = {
-    name: "Pick up drycleaning",
-    state: TodoState.Complete
+class SmartTodo {
+    
+    _state: TodoState;
+    
+    name: string;
+    
+    get state() {
+        return this._state;
+    }
+    
+    set state(newState) {
+        
+        if(newState == TodoState.Complete) {
+            
+            var canBeCompleted = 
+                this.state == TodoState.Active
+                || this.state == TodoState.Deleted;
+                
+            if(!canBeCompleted) {
+                throw "Todo must be Active or Deleted in order to be marked Completed"
+            }
+        }
+        
+        this._state = newState;
+    }
+    
+    constructor(name: string) {
+        this.name = name;
+    }
 }
+
+var todo = new SmartTodo("Pick up drycleaning");
+
+todo.state = TodoState.Complete;
+
+todo.state
