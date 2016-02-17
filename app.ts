861b80cf49c64b4c93377855642c207b98560ede
Jess Chadwick <jesschadwick@gmail.com>
2016-02-16 21:12:33 -0500

    05_07_Start

--- app.ts
+++ app.ts
@@ -10,21 +10,51 @@ enum TodoState {
     Deleted
 }
 
+
+class TodoService {
+
+    static lastId: number = 0;
+
+    get nextId() {
+        return TodoService.getNextId();
+    }
+
+    set nextId(nextId) {
+        TodoService.lastId = nextId - 1;
+    }
+
+    constructor(private todos: Todo[]) {
+    }
+
+    add(todo: Todo) {
+        var newId = this.nextId;
+    }
+
+    getAll() {
+        return this.todos;
+    }
+
+    static getNextId() {
+        return TodoService.lastId += 1;
+    }
+}
+
+
 abstract class TodoStateChanger {
-    
+
     constructor(private newState: TodoState) {
     }
-    
+
     abstract canChangeState(todo: Todo): boolean;
-    
+
     changeState(todo: Todo): Todo {
-        if(this.canChangeState(todo)) {
+        if (this.canChangeState(todo)) {
             todo.state = this.newState;
         }
-        
+
         return todo;
     }
-    
+
 }
 
 class CompleteTodoStateChanger extends TodoStateChanger {
@@ -32,13 +62,21 @@ class CompleteTodoStateChanger extends TodoStateChanger {
     constructor() {
         super(TodoState.Complete);
     }
-    
+
     canChangeState(todo: Todo): boolean {
         return !!todo && (
-              todo.state == TodoState.Active
-           || todo.state == TodoState.Deleted
+            todo.state == TodoState.Active
+            || todo.state == TodoState.Deleted
         )
     }
-    
+
 }
 
+
+class SmartTodo {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
