27b62d9507842ee0afed72f85856bd458bde2ec3
Jess Chadwick <jesschadwick@gmail.com>
2016-02-17 01:31:33 -0500

    06_01_End

--- app.ts
+++ app.ts
@@ -1,4 +1,18 @@
-function clone(value) {
+function clone<T>(value: T): T {
     let serialized = JSON.stringify(value);
     return JSON.parse(serialized);
 }
+
+clone('Hello!')
+
+clone(123)
+
+var todo: Todo = {
+    id: 1,
+    name: 'Pick up drycleaning',
+    state: TodoState.Active
+};
+
+clone(todo)
+
+clone({ name: 'Jess' })
