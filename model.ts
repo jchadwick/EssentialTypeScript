6fee111fc896df764c56e342183eb207ea2fbc75
Jess Chadwick <jesschadwick@gmail.com>
2016-02-22 01:42:09 -0500

    07_02_End

--- model.ts
+++ model.ts
@@ -1,22 +1,34 @@
+namespace TodoApp.Model {
 
+    export interface Todo {
+        id: number;
+        name: string;
+        state: TodoState;
+    }
 
-interface Todo {
-    id: number;
-    name: string;
-    state: TodoState;
 }
 
-/* START: TodoState */
-enum TodoState {
-    New = 1,
-    Active,
-    Complete,
-    Deleted
+namespace TodoApp.Model {
+
+    export enum TodoState {
+        New = 1,
+        Active,
+        Complete,
+        Deleted
+    }
 }
 
-interface ITodoService {
-    add(todo: Todo): Todo;
-    delete(todoId: number): void;
-    getAll(): Todo[];
-    getById(todoId: number): Todo;
+namespace DataAccess {
+    
+    import Model = TodoApp.Model;
+    import Todo = Model.Todo;
+
+    export interface ITodoService {
+        add(todo: Todo): Todo;
+        delete(todoId: number): void;
+        getAll(): Todo[];
+        getById(todoId: number): Todo;
+    }
+
 }
+
